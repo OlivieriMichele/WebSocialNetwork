e9d10473d80399821046bd611f552a68e1a96ea4
Michele Olivieri <micheleolivieri02@gmail.com>
2024-01-06 13:52:29 +0100

    implement nav tabs with js

--- src/js/homeNLog.js
+++ src/js/homeNLog.js
@@ -1,5 +1,32 @@
-const next = document.querySelector(".next");
+const allLinks = document.querySelectorAll("nav ul a");
+const allTabs = document.querySelectorAll(".tab-content");
 
-next.addEventListener('change', ()=>{
-    document.body.classList.toggle("painting");
+const shiftTabs = (linkId) => {
+    allTabs.forEach( (tab, i) => {
+        
+        if(tab.id.includes(linkId)) {
+            allTabs.forEach( (tabItem) => {
+                tabItem.style = `transform: translateY(-${i*300}px;)`;
+            });
+        }
+    });
+}
+
+allLinks.forEach((elem) => {
+    elem.addEventListener('click', function(){
+        const linkId = elem.id;
+        const hrefLinkClick = elem.href;
+
+        allTabs.forEach((elem) => {
+            if(elem.id.includes(linkId)){
+                elem.classList.add('active');
+                elem.classList.remove('disable');
+            } else {
+                elem.classList.remove('active');
+                elem.classList.add('disable');
+            }
+        });
+
+        shiftTabs(linkId);
+    });
 });
