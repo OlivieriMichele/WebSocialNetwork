13ea7e18b1149797351cf391dcb4ddd7b1ce87a9
Michele Olivieri <micheleolivieri02@gmail.com>
2024-01-08 10:30:16 +0100

    implement handle for initial load

--- src/js/homeNLog.js
+++ src/js/homeNLog.js
@@ -1,21 +1,10 @@
 const allLinks = document.querySelectorAll("nav ul a");
 const allTabs = document.querySelectorAll(".tab-content");
-
-const shiftTabs = (linkId) => {
-    allTabs.forEach( (tab, i) => {
-        
-        if(tab.id.includes(linkId)) {
-            allTabs.forEach( (tabItem) => {
-                tabItem.style = `transform: translateY(-${i*300}px;)`;
-            });
-        }
-    });
-}
+const body = document.querySelector("body");
 
 allLinks.forEach((elem) => {
     elem.addEventListener('click', function(){
         const linkId = elem.id;
-        const hrefLinkClick = elem.href;
 
         allTabs.forEach((elem) => {
             if(elem.id.includes(linkId)){
@@ -26,7 +15,5 @@ allLinks.forEach((elem) => {
                 elem.classList.add('disable');
             }
         });
-
-        shiftTabs(linkId);
     });
 });
